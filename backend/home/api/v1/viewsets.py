c2f657cc6d2bc018bb4e72ffeea97c55b6f646d3
Crowdbotics <team@crowdbotics.com>
2022-04-27 15:08:27 +0000

    Update models

--- backend/home/api/v1/viewsets.py
+++ backend/home/api/v1/viewsets.py
@@ -1,3 +1,7 @@
+from rest_framework import viewsets
+from home.models import Day
+from .serializers import DaySerializer
+from rest_framework import authentication
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.viewsets import ModelViewSet, ViewSet
 from rest_framework.authtoken.models import Token
@@ -28,3 +32,12 @@ class LoginViewSet(ViewSet):
         token, created = Token.objects.get_or_create(user=user)
         user_serializer = UserSerializer(user)
         return Response({"token": token.key, "user": user_serializer.data})
+
+
+class DayViewSet(viewsets.ModelViewSet):
+    serializer_class = DaySerializer
+    authentication_classes = (
+        authentication.SessionAuthentication,
+        authentication.TokenAuthentication,
+    )
+    queryset = Day.objects.all()
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,13 @@
+from django.conf import settings
 from django.db import models
+
+
+class Day(models.Model):
+    "Generated Model"
+    event1 = models.CharField(
+        max_length=256,
+    )
+    event2 = models.CharField(
+        max_length=256,
+    )
+    addEvent = models.BooleanField()
